c61306533efe63c6dcb3e72cbc1d72ee9d53e32a
sampath <sampathbandla@lewisu.edu>
2022-11-02 18:07:42 +0530

    Intitial Commit

---
+++ js/profile.js
@@ -0,0 +1,38 @@
+$("document").ready(() => {
+    $("#loading").removeClass("d-none");
+    $.ajax({
+        url: "https://randomuser.me/api/",
+        dataType: "json",
+        async: true,
+        success: function (data) {
+            let profile = data.results[0];
+            // We got data
+            $("#profileImage").attr("src", profile.picture.large);
+            $("#profileName").text(
+                profile.name.first + " " + profile.name.last
+            );
+            $("#profileLocation").text(profile.location.country);
+            $("#profileEmail").text(profile.email);
+            $("#profilePhone").text(profile.phone);
+            $("#profileStreet").text(
+                profile.location.street.number +
+                    " " +
+                    profile.location.street.name
+            );
+            $("#profileCity").text(profile.location.city);
+            $("#profileState").text(profile.location.state);
+            $("#profileCountry").text(profile.location.country);
+            $("#profilePostCode").text(profile.location.postcode);
+            $("#profileUsername").text(profile.login.username);
+            $("#profilePassword").text(profile.login.password);
+            $("#profileDob").text(new Date(profile.dob.date).toDateString());
+            $("#profileAge").text(profile.dob.age);
+            $("#profileSSN").text(profile.id.value || "Not available");
+
+
+
+            // Hiding loading after loading data
+            $("#loading").addClass("d-none");
+        },
+    });
+});
